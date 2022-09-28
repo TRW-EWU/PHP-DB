631abc5c85670dfb7f12408b80da5baea0ddef44
twalsh@ewu.edu <twalsh@ewu.edu>
2022-09-28 18:10:42 -0400

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My blog</title>
+        <meta charset="utf-b">
+    </head>
+    <body>
+        <header>
+            <h1>My blog</h1>
+        </header>
+
+        <main>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+// This is a test for git
+// Another entry
+// Change #2
+//
+// Changes on TestBranch in GIT
+
+//
+// Change #3
+//
+require 'includes/database.php';
+
+$conn = getDB();
+
+$sql = "SELECT *
+        FROM article
+        ORDER BY published_at;";
+
+$results = mysqli_query($conn, $sql);
+
+if ( $result === false) {
+    echo mysqli_error($conn);
+} else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+}
+?>
+
+<?php require 'includes/header.php'; ?>
+
+<a href="new-article.php">New article</a>
+
+            <?php if (empty($articles)): ?>
+                <p>No articles found.</p>
+            <?php else: ?>
+                <ul>
+                    <?php foreach ($articles as $article): ?>
+                        <li>
+                            <article>
+                                <h2>
+                                    <a href="article.php?id=<?= $article['id'] ?>" >
+                                        <?= htmlspecialchars($article['title']); ?>
+                                    </a>
+                                </h2>
+                                <p><?= htmlspecialchars($article['content']); ?></p>
+                            </article>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+
+<?php require 'includes/footer.php'; ?>
