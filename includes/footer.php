96d0e57cd97a0a98eef90c87173cd5c4b7d0e395
Thomas Walsh <twalsh@ewu.edu>
2022-10-09 13:41:21 -0400

    Section #25 Completed

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,11 @@
         <script src="https://code.jquery.com/jquery-3.6.1.min.js"
                 integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
                 crossorigin="anonymous"></script> 
-                
+
+        <script 
+            src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js">
+        </script>
+        
         <script src="/js/script.js"></script>
     </body>
 </html>
